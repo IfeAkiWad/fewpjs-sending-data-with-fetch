b0f36b3e1416dbc8ebb1a1f60c6a49347360ebb9
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-02-25 18:09:03 -0600

    commit tree cc090cba7852797addee97ba77a99d8b5c6b1757

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+
+function submitData(userName, email) {
+    let formData = {
+        name: "Steve",
+        email: "steve@steve.com"
+    }
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+    
+    return fetch('http://localhost:3000/users', configObj)
+    .then(response => response.json())
+    .then(users => {
+        console.log(users)
+        document.body.innerHTML = object["id"]
+    })
+    
+}
